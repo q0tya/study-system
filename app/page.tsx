a53065822b101d580d9a6d0c104edf41d0650fa5
q0tya <ceremnovkosta@gmail.com>
2025-12-17 23:25:44 +0300

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-gray-500 dark:text-gray-400">© 2025 Учебный проект q0tya. Все права защищены.</p>
+        <p className="text-xs text-gray-500 dark:text-gray-400">© 2025 Учебный проект q0tya и s1tov. Все права защищены.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Пользовательское соглашение
